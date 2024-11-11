ba8665d7eebe0cdd18a42980538074874517e820
Ibrahim-Lokman <ibrahimlokman.bd@gmail.com>
2024-11-11 17:13:22 +0600

    Worked with Star Rating component.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import StarRating from "./StarRating";
-// import "./index.css";
-// import App from "./App";
+// import StarRating from "./StarRating";
+import "./index.css";
+import App from "./App";
 // import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-
-    <StarRating maxRating={10} />
+    <App />
+    {/* <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} className="test" defaultRating={2} /> */}
   </React.StrictMode>
 );
