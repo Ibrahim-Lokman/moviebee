eb3492d6e78a56cff3677aa2d65b07466a5d1b56
Ibrahim-Lokman <ibrahimlokman.bd@gmail.com>
2024-11-15 10:59:45 +0600

    Fecting movie data from api. (infinite fetching problem)

--- src/App-v1.js
+++ src/App-v1.js
@@ -49,14 +49,10 @@ const tempWatchedData = [
 
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
-const KEY = "9a044df6";
-export default function App() {
-  const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
 
-  fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
-    .then((res) => res.json())
-    .then((data) => setMovies(data.Search));
+export default function App() {
+  const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
   return (
     <>
       <NavBar>
