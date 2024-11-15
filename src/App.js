b4e89d164055e55aff8c25283314c5ccb64f6b87
Ibrahim-Lokman <ibrahimlokman.bd@gmail.com>
2024-11-15 21:38:01 +0600

    Call handleCloseMovie before fetching movies to ensure proper state management

--- src/App.js
+++ src/App.js
@@ -109,6 +109,7 @@ export default function App() {
         setError("");
         return;
       }
+      handleCloseMovie();
       fetchMovies();
       return function () {
         controller.abort();
